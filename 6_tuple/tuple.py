2513f4434ed486c781b62e73c31c6dcba1e5f12b
Diego Graciano <159558560+oliverdeath666@users.noreply.github.com>
2025-05-28 08:13:27 -0600

    Delete 6_tuple directory

--- 6_tuple/tuple.py
+++
@@ -1,7 +0,0 @@
-'''
-Las tuplas se utilizan para almacenar varuois elementi ej una sola variable
-
-una tupla es una coleccion oredenada a inmutables 
-
-las tuplas se escribem entre paerentediddi
-'''
